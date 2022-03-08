73191d6af3d0b589f76ba124c85a03f5985f67d2
Sinchal <94883181+Sinchal@users.noreply.github.com>
2022-03-08 15:30:21 +0000

    done

--- p2final.c
+++ p2final.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
+
 typedef struct _fraction 
 {
     int num,den;
 }F;
+
 F input_fraction()
 {
     F f;
-    printf("enter num,den: \n ");
+    printf("enter the numerator and denominator \n ");
     scanf("%d%d",&f.num,&f.den);
     return f;
 }
+
 F smallest_fraction(F f1,F f2,F f3)
 {
     int cd=f1.den*f2.den*f3.den;
@@ -29,21 +32,12 @@ F smallest_fraction(F f1,F f2,F f3)
         return f3;
     }
 }
+
 void output(F f1,F f2,F f3,F smallest)
 {
-    if(f1.num==smallest.num && f1.den==smallest.den)
-    {
-        printf("the smallest fraction among %d/%d , %d/%d and %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,smallest.num,smallest.den);
-    }
-    else if(f2.num==smallest.num && f2.den==smallest.den)
-    {
-         printf("the smallest fraction among %d/%d , %d/%d and %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,smallest.num,smallest.den);
-    }
-    else
-    {
-         printf("the smallest fraction among %d/%d , %d/%d and %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,smallest.num,smallest.den);
-    }
+  printf("the smallest fraction among %d/%d , %d/%d and %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,smallest.num,smallest.den);
 }
+ 
 int main()
 {
     F f1,f2,f3,smallest;
--- p3final.c
+++ p3final.c
@@ -1,48 +1,36 @@
-/*
-Write a program find whether a given number is a prime number.
-int input_number();
-int is_prime(int n);
-void output(int n, int is_prime);
-*/
-
 #include <stdio.h>
-#include <math.h>
-int input()
+
+void input_n_and_r(int *n, int *r)
 {
-  int n;
-  printf("Enter the number\n");
-  scanf("%d",&n);
-  return n;
+  printf("enter n and r\n");
+  scanf("%d%d",n,r);
 }
 
-int isprime(int n)
-{
-  if (n==0 || n==1) {
-    return 0
-  }
-  if (n==2) {
-    return 1;
-  }
-  for(int i= 2; i<= sqrt(n);i++)
-  {
-    if(n%i==0)
-      return 0;
-  }
-  return 1;
+int factorial(int n)
+ {
+	int factorial = 1;
+	for (int i = 2; i <= n; i++)
+		factorial = factorial * i;
+	return factorial;
 }
-void output(int n, int isp)
+
+int ncr(int n, int r)
 {
-  if(isp == 0)
-    printf("%d is not a prime number\n",n);
-  else
-    printf("%d is a prime number\n",n)
+	return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+void output(int n, int r, int result)
+{
+  printf("the ncr of %d and %d is %d",n,r,result);
 }
 
 int main()
 {
-  int n = input();
-  int isp = isprime(n);
-  output(n,isp);
-  return 0;
-  }
+	int n ,r,f,result;
+  input_n_and_r(&n,&r);
+  f=factorial(n);
+  result=(n,r);
+	result=ncr(n, r);
+  output(n,r,result);
+	return 0;
 }
