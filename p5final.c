aafd9dbcacd566b6a921aa8995fc50fad7638bc7
Sinchal <94883181+Sinchal@users.noreply.github.com>
2022-03-08 16:58:51 +0000

    done

--- p5final.c
+++ p5final.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
-int input()
+
+int input_size()
 {
   int n;
-  printf("Enter the number\n");
+  printf("Enter how many numbers you want to compare\n");
   scanf("%d",&n);
   return n;
 }
 
-void init_array(int n, int a[n])
+void input_array(int n, int a[n])
 {
-  for(int i=0;i<n;i++)
-    a[i] = i;
-  a[1]=0;
+ for(int i=0;i<n;i++)
+ {
+   printf("Enter the element no %d of the array\n",i);
+   scanf("%d",&a[i]);
+ }
 }
 
-void ets(int n, int a[n])
+int find_largest(int n, int a[n])
 {
-  int i=0;
-   /* Find next non-zero number */
-  while(i<sqrt(n)){
-   for(;a[i]==0 ;i++);
-   for(int k=i+i;k<n;k += i)
-     a[i] = 0;
-   i++;
+  int max = a[0];
+  int largest = 0;
+  for (int i = 0; i < n; i++) 
+  {
+   if (max < a[i]) 
+   {
+     max = a[i];
+     largest = i;
+   }
   }
+  return largest;
 }
 
-
-void display(int n, int a[n])
+void out_put(int n, int a[n], int largest)
 {
-  for(int i=0;i<n;i++)
-    if(a[i]!=0)
-      printf("%d ",a[i]);
-  printf("\n");
+  printf("Index position of Maximum value in the array is  : %d\n",largest);
 }
 
 int main()
 {
-  int n;
-  n=input();
-  int a[n];
-  init_array(n,a);
-  ets(n,a);
-  display(n,a);
+  int n,a[100],largest;
+  n=input_size();
+  input_array(n,a);
+  largest=find_largest(n,a);
+  out_put(n,a,largest);
   return 0;
 }
